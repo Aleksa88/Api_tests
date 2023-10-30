6f33445d537d940997d6f2308d3247b1faeac1d7
Aleksa88 <76181764+Aleksa88@users.noreply.github.com>
2023-10-30 11:11:12 +0300

    исправлены ошибки

--- src/test/java/com/ors/api/test/CreateEmployeeTest.java
+++ src/test/java/com/ors/api/test/CreateEmployeeTest.java
@@ -1,35 +1,61 @@
 package com.ors.api.test;
 
 import com.ors.ConfigReader;
+import com.ors.JsonDataUpdater;
 import com.ors.TokenProvider;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class CreateEmployeeTest {
     private static String accessToken;
 
     @BeforeAll
     public static void setup() {
-        ConfigReader configReader = new ConfigReader(); // Создаем экземпляр ConfigReader
-        TokenProvider tokenProvider = new TokenProvider(configReader); // Создаем экземпляр TokenProvider с передачей ConfigReader
-        accessToken = tokenProvider.getAuthToken(); // Вызываем метод getAuthToken() для получения access token
+        ConfigReader configReader = new ConfigReader();
+        TokenProvider tokenProvider = new TokenProvider(configReader);
+        accessToken = tokenProvider.getAuthToken();
     }
 
     @Test
     public void testCreateEmployee() {
-        String jsonInput = "{ \"phone\": \"79456543456\", \"email\": \"Qwerty123.@mail.ru\", \"first_name\": \"Апишный\"," +
-                " \"middle_name\": \"Тестович\", \"last_name\": \"Тест\", \"company_id\": 1, " + "\"house_group_id\": 1, " +
-                "\"is_blocked\": false, \"role_ids\": [ 2968 ], \"house_groups\": [], \"abilities\": [ \"view\", \"edit\" ], \"is_co_executor\": true }";
+        try {
+            // Чтение содержимого файла random_data.json
+            String filePath = "src/main/resources/random_data.json";
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            StringBuilder jsonInputBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonInputBuilder.append(line);
+            }
+            reader.close();
+            String jsonInput = jsonInputBuilder.toString();
 
-        RestAssured.given()
-                .header("Authorization", "Bearer " + accessToken) // Добавляем заголовок авторизации
-                .contentType(ContentType.JSON)
-                .body(jsonInput)
-                .when()
-                .post("https://api.gkh911.ru/api/employee/employees")
-                .then()
-                .statusCode(201);
+            RestAssured.given()
+                    .header("Authorization", "Bearer " + accessToken)
+                    .contentType(ContentType.JSON)
+                    .body(jsonInput)
+                    .when()
+                    .post("https://api.gkh911.ru/api/employee/employees")
+                    .then()
+                    .statusCode(201)
+                    .log().all()
+                    .extract().response();
+
+            System.out.println("Сотрудник успешно создан!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    @AfterEach
+    public void teardown() {
+        JsonDataUpdater.main(null);
+    }
+
 }
